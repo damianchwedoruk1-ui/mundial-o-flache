aed693e4c36040bc00983d3c16a15d46dfecafc6
Damian <damian@test.pl>
2026-06-06 17:54:05 +0200

    move test to next match day

--- lib/demoData.ts
+++ lib/demoData.ts
@@ -76,11 +76,12 @@ export const worldCupTeams = [
 ];
 
 
+// TEST_DAY_SHIFT_2026_06_06: aktualny dzien testowy to mecze 5-8
 export const demoMatches = [
   {
     id: 1,
     group: "Grupa A",
-    date: "06.06.2026",
+    date: "05.06.2026",
     teamA: "Meksyk",
     teamB: "Republika Południowej Afryki",
     time: "18:00",
@@ -89,7 +90,7 @@ export const demoMatches = [
   {
     id: 2,
     group: "Grupa A",
-    date: "06.06.2026",
+    date: "05.06.2026",
     teamA: "Korea Południowa",
     teamB: "Czechy",
     time: "20:00",
@@ -98,7 +99,7 @@ export const demoMatches = [
   {
     id: 3,
     group: "Grupa B",
-    date: "06.06.2026",
+    date: "05.06.2026",
     teamA: "Kanada",
     teamB: "Bośnia i Hercegowina",
     time: "21:00",
@@ -107,7 +108,7 @@ export const demoMatches = [
   {
     id: 4,
     group: "Grupa D",
-    date: "06.06.2026",
+    date: "05.06.2026",
     teamA: "USA",
     teamB: "Paragwaj",
     time: "23:00",
@@ -146,42 +147,6 @@ export const demoMatches = [
     date: "07.06.2026",
     teamA: "Australia",
     teamB: "Turcja",
-    time: "23:30",
-  },
-
-  {
-    id: 9,
-    group: "Grupa E",
-    date: "08.06.2026",
-    teamA: "Niemcy",
-    teamB: "Japonia",
-    time: "18:00",
-  },
-
-  {
-    id: 10,
-    group: "Grupa E",
-    date: "08.06.2026",
-    teamA: "Francja",
-    teamB: "Senegal",
-    time: "20:00",
-  },
-
-  {
-    id: 11,
-    group: "Grupa F",
-    date: "08.06.2026",
-    teamA: "Argentyna",
-    teamB: "Portugalia",
-    time: "22:00",
-  },
-
-  {
-    id: 12,
-    group: "Grupa F",
-    date: "08.06.2026",
-    teamA: "Anglia",
-    teamB: "Holandia",
     time: "23:30",
   },
 ];
